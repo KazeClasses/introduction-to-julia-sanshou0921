27ceaa4fd4b3b13a5e70ece23b5847466818dac2
sanshou0921 <shianjie009@gmail.com>
2024-09-19 19:15:52 -0400

    Update InsertionSort.jl

--- src/InsertionSort.jl
+++ src/InsertionSort.jl
@@ -1,29 +1,3 @@
 module InsertionSort
-
-using Test
-using Random
-
-function insertion_sort!(array::Array{Int, 1}) where Int
-    # Insert your code here
-    n = length(arr)
-    for i in 2:n
-        key = arr[i]
-        j = i-1
-        where j > 0 && arr[j] > key
-        arr[j+1] = arr[j]
-        j -=1
-    end
-    arr[j+1] = key
+include("insertion_sort.jl")
 end
-
-function check_sort(N::Int = 100)
-    array = collect(1:N)
-    shuffle!(array)
-    insertion_sort!(array)
-    return array == collect(1:N)
-end
-
-export insertion_sort!, check_sort
-end
-
-export insertion_sort!
